83c811750f3e12ee2a2c0195e5a9ca1bbbfce882
Shahlo Suvonqulova <shahlosuvonqulova@gmail.com>
2023-12-23 21:03:21 -0500

    changed file

--- variables.tf
+++ variables.tf
@@ -3,25 +3,25 @@ variable "security_groups" {
   type = map(object({
     description = string
     ingress_rules = optional(list(object({
-      description = optional(string)
-      priority    = optional(number)
-      from_port   = number
-      to_port     = number
-      protocol    = string
-      cidr_blocks = optional(list(string))
+      description     = optional(string)
+      priority        = optional(number)
+      from_port       = number
+      to_port         = number
+      protocol        = string
+      cidr_blocks     = optional(list(string))
       security_groups = optional(list(string))
     })))
-    egress_rules = list(object({
-      description = optional(string)
-      priority    = optional(number)
-      from_port   = number
-      to_port     = number
-      protocol    = string
-      cidr_blocks = optional(list(string))
+    egress_rules = optional(list(object({
+      description     = optional(string)
+      priority        = optional(number)
+      from_port       = number
+      to_port         = number
+      protocol        = string
+      cidr_blocks     = optional(list(string))
       security_groups = optional(list(string))
-    }))
+    })))
   }))
-  default = {}      
+  default = {}
 }
 
 variable "vpc_id" {
